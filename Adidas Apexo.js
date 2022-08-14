25e3264de665c254ec64ad90ec4a9fd334f686e6
Repro12 <111174041+Repro12@users.noreply.github.com>
2022-08-14 05:01:58 -0700

    Add files via upload

---
+++ Adidas Apexo.js
@@ -0,0 +1,12 @@
+
+function amazon(){
+    window.location.href= "https://www.amazon.in/Adidas-Apexo-White-Sonic-GA1158/dp/B096NFG7KF/ref=asc_df_B096NFG7KF/?tag=googleshopdes-21&linkCode=df0&hvadid=598160042634&hvpos=&hvnetw=g&hvrand=7067497320490826893&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007745&hvtargid=pla-1658667689495&psc=1";
+};
+
+function ajio(){
+    window.location.href= "https://www.ajio.com/adidas-apexo-m-lace-up-walking-shoes/p/469107262_white?gclid=CjwKCAjwi8iXBhBeEiwAKbUofckLsLAIdq9pfalSQNqmy4rc0EFYgxwlRjRs1VOlKDG7IO2Okkzh4xoC4M0QAvD_BwE#gmf";
+};
+
+function adidas(){
+    window.location.href= "https://www.adidas.co.in/apexo-m/GA1158.html?cm_mmc=AdieSEM_Google-_-%7Bcampaign%7D-_-%7Badgroup%7D-_-&cm_mmca1=IN&cm_mmca2=&gclid=CjwKCAjwi8iXBhBeEiwAKbUofelwcBwMiWZWV2yw4pDqWN1wU2j5opFHTQJHyLWLmELO6x5XNe6yFBoCagkQAvD_BwE";
+};
